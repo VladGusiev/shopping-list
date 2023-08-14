a16bfbbf024949db5a482c6cc86e22f61ebb95ea
Vladyslav Gusiev <vladyslav.gusiev@gmail.com>
2023-08-14 18:00:42 +0200

    dark/light mode added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,25 @@
 import "../styles/Header.css";
+import {useState} from "react";
 function Header() {
+
+    const [isDark, setIsDark] = useState(false);
+
+    function toggleTheme() {
+        if (!isDark) {
+            document.body.classList.add('dark');
+            setIsDark(true);
+        } else {
+            document.body.classList.remove('dark');
+            setIsDark(false);
+        }
+    }
 
     return (
         <div className="header-wrapper">
             <h1>Shopping List</h1>
             <button
-                className="theme-switch">🌙</button>
+                onClick={toggleTheme}
+                className="theme-switch">{isDark ? "☀️" : "🌙"}</button>
         </div>
     )
 }
